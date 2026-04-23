9ea906e7a7c44ff0674608bfa4a8c9e9cf7d2dda
Siva sai teja <gurramsai02@gmail.com>
2026-04-23 17:46:03 +0530

    Update script.sh

--- infra/script.sh
+++ infra/script.sh
@@ -1,41 +1,48 @@
 #!/bin/bash
 set -e
 
-# 1. Set variables globally (This makes them survive reboots)
+# Update packages
+sudo apt update -y
+
+# Install required packages
+sudo apt install -y python3 python3-pip python3.12-venv git
+
+# Export DB variables
 echo "DB_HOST=${db_host}" | sudo tee -a /etc/environment
 echo "DB_NAME=${db_name}" | sudo tee -a /etc/environment
 echo "DB_USER=${db_user}" | sudo tee -a /etc/environment
 echo "DB_PASSWORD=${db_pass}" | sudo tee -a /etc/environment
 
-# 2. Export them for this specific execution session
 export DB_HOST=${db_host}
 export DB_NAME=${db_name}
 export DB_USER=${db_user}
 export DB_PASSWORD=${db_pass}
 
-# System Update
-sudo apt update -y
-sudo apt install -y python3 python3-pip python3-venv git
-sudo apt install -y python3-venv python3-pip
-
+# Go project folder
 cd /home/ubuntu
-git clone https://github.com/Sivasaiteja2/AWS-2-Tier-Student-Registration-System
-cd AWS-2-Tier-Student-Registration-System
-cd student-app
 
-sudo apt update
-sudo apt install -y python3.12-venv python3-pip
+# Clone if not exists
+if [ ! -d AWS-2-Tier-Student-Registration-System ]; then
+    git clone https://github.com/Sivasaiteja2/AWS-2-Tier-Student-Registration-System
+fi
 
-python3 --version
+cd AWS-2-Tier-Student-Registration-System/student-app
 
+# Remove old venv
 rm -rf venv
+
+# Create new virtual env
 python3 -m venv venv
 
-. venv/bin/activate
+# Activate
+source venv/bin/activate
 
+# Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Kill old gunicorn
 pkill gunicorn || true
 
-nohup gunicorn -w 2 -b 0.0.0.0:5000 app > app.log 2>&1 &
+# Start app
+nohup gunicorn -w 2 -b 0.0.0.0:5000 app:app > app.log 2>&1 &
